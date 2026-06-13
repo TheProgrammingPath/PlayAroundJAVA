baf3bf20a3fbf9f21961ab132e86495e22d9fb0e
TheProgrammingPath <mtoure7@mail.dccc.edu>
2026-06-13 00:32:21 -0400

    Add files via upload

---
+++ Birthday.java
@@ -0,0 +1,310 @@
+
+
+public class Huh {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        if(x<y) System.out.println("Mohameds a small boy");
+        x = x * 2;
+        if(x==y) System.out.println("Mohamed has an equator");
+        x = x * 2;
+        if(x>y) System.out.println("Mohameds now bigger than the small boy");
+        if(x==y) System.out.println("Your not excused");
+
+        System.out.println("\n");
+
+        int me;
+        for(me=0; me<10; me++);
+        System.out.println("The Birthday man is evolving");
+
+        System.out.println("\n");
+
+
+    int m,t;
+    t = 20;
+    for(m=0; m<10; m++){
+        System.out.println("Evolution at its finest: " + m);
+        System.out.println("Gets finer and finer: " + t);
+        t = t + 2;
+    }
+
+    System.out.println("\n");
+
+    double pi, r, a;
+    r = 10.8;
+    pi = 3.1416;
+    a = pi * r * r;
+    System.out.println("Mohamed is flying up up and away like: " + a);
+
+    System.out.println("\n");
+
+    int gasher;
+    long days;
+    long seconds;
+    long distance;
+
+    gasher = 186000;
+    days = 50000;
+    seconds = days * 24 * 60 * 60;
+    distance = gasher * seconds;
+    System.out.print("In " + days);
+    System.out.print(" days Mohame will have a senior project thats worth about ");
+    System.out.println(distance + " thats company ready.");
+
+
+    System.out.println("\n");
+
+    char java,mohamed;
+    java = 88;
+    mohamed = 'Y';
+    System.out.print("java and mohamed are like: ");
+    System.out.println(java + " " + mohamed);
+
+    System.out.println("\n");
+
+    boolean j;
+    j = false;
+    System.out.println("Mohamed is " + j);
+    j = true;
+    System.out.println("Mohamed is " + j);
+    if(j) System.out.println("This is executed.");
+    j = false;
+    if(j) System.out.println("This isnt executed.");
+    System.out.println("Mohamed > small Mohamed is " + (10 > 9));
+
+    System.out.println("\n");
+
+    char ch3;
+    ch3 = 'X';
+    System.out.println("The past mohamed contains: " + ch3);
+    ch3++;
+    System.out.println("The Future mohamed is now: " + ch3);
+
+    System.out.println("\n");
+
+    double u=3.0,z=4.0;
+    double pig = Math.sqrt(u * u + z * z);
+    System.out.println("My gpa goal amount is about " + pig);
+
+    System.out.println("\n");
+    
+    int o;
+    o = 22;
+    if(o == 22){
+        int d = 23;
+        System.out.println("PastMohamed and CurrentMohamed " + o + " " + d);
+        o = d * 2;
+        System.out.println("Ill be Senior Java developer by " + o);
+    }
+
+    System.out.println("\n");
+
+    int Swirly;
+    for(Swirly=0; Swirly<3; Swirly++){
+        int s = -1;
+        System.out.println("Mohamed will not represent " + s);
+        s = 100;
+        System.out.println("Mohamed will feel more like a " + s);
+    }
+
+    System.out.println("\n");
+
+    byte ginza;
+    int lostfiles = 257;
+    double dmgg = 323.142;
+    System.out.println("\nClarification");
+    ginza = (byte) lostfiles;
+    System.out.println("Lets make history Mohamed " + lostfiles + " " + ginza);
+    System.out.println("\n");
+    lostfiles = (int) dmgg;
+    System.out.println("ooooooo " + dmgg + " " + lostfiles);
+    System.out.println("\nShooting to the stars ");
+    lostfiles = (byte) dmgg;
+    System.out.println("Dont look back " + dmgg + " " + lostfiles);
+
+    System.out.println("\n");
+
+    byte b = 42;
+    char c = 'a';
+    short s = 1024;
+    int i = 50000;
+    float f = 5.67f;
+    double d = .1234;
+    double solution = (f * b) + (i / c) - (d * s);
+    System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+    System.out.println("result = " + solution);
+
+    System.out.println("\n");
+
+    int month_days[];
+    month_days = new int[12];
+    month_days[0] = 31;
+    month_days[1] = 28;
+    month_days[2] = 31;
+    month_days[3] = 30;
+    month_days[4] = 31;
+    month_days[5] = 30;
+    month_days[6] = 31;
+    month_days[7] = 30;
+    month_days[8] = 31;
+    month_days[9] = 30;
+    month_days[10] = 31;
+    month_days[11] = 30;
+    System.out.println("June has " + month_days[3] + " days, with mohameds birthday being on the 11th." );
+
+    System.out.println("\n");
+
+    double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
+    double result = 0;
+    int n;
+
+    for(n=0; n<5; n++)
+        result = result + nums[n];
+        System.out.println("Scoring out 12 on projects, Mohamed Average will be " + result / 5);
+
+        System.out.println("\n");
+
+               int twoD[][] = new int[4][];
+               twoD[0] = new int[1];
+               twoD[1] = new int[2];
+               twoD[2] = new int[3];
+               twoD[3] = new int[4];
+        int armored, vitamin, star = 0;
+
+        for(armored=0; armored<4; armored++)
+            for(vitamin=0; vitamin<armored+1; vitamin++){
+        twoD[armored][vitamin] = star;
+        star++;
+        }
+
+        for(armored=0; armored<4; armored++){
+            for(vitamin=0; vitamin<armored+1; vitamin++)
+            System.out.print(twoD[armored][vitamin] + " ");
+            System.out.println("Mohamed The best");
+        }
+
+        System.out.println("\n");
+        double Mohamed[][] = {
+            {0*0, 0*1, 0*2, 0*3},
+            {0*1, 1*1, 2*1, 3*1},
+            {0*2, 1*2, 2*2, 3*2},
+            {0*3, 1*3, 2*3, 3*3}
+        };
+        int q,h;
+
+        for(q=0; q<4; q++){
+            for(h=0; h<4; h++)
+                System.out.print(Mohamed[q][h] + " ");
+            System.out.println("Aiming to be a Network/Software engineering");
+        }
+
+        System.out.println("\n");
+
+        int threeD[][][] = new int[3][4][5];
+        int starlett, benz, jacket;
+
+        for(starlett=0; starlett<3; starlett++)
+            for(benz=0; benz<4; benz++)
+        for(jacket=0; jacket<5; jacket++)
+    threeD[starlett][benz][jacket] = starlett * benz * jacket;
+
+        for(starlett=0; starlett<3; starlett++){
+            for(benz=0; benz<4; benz++){
+                for(jacket=0; jacket<5; jacket++)
+                    System.out.print(threeD[starlett][benz][jacket] + " ");
+                System.out.println("Just dimenonsiol");
+            }
+            System.out.println("");
+        }
+
+        int study = 1 + 1;
+        int be = study * 3;
+        int ce = be / 4;
+        int de = ce - study;
+        int ee = -de;
+        System.out.println("study = " + study);
+        System.out.println("be = " + be);
+        System.out.println("ce = " + ce);
+        System.out.println("de = " + de);
+        System.out.println("ee = " + ee);
+        System.out.println("Floating Point Arithmetic");
+        double dstudy = 1 + 1;
+        double dbe = dstudy * 3;
+        double dce = dbe / 4;
+        double dde = dce - study;
+        double dee = -dde;
+        System.out.println("double dstudy = " + dstudy);
+        System.out.println("doube dbe = " +  dbe);
+        System.out.println("double dce = " + dce);
+        System.out.println("double dde = " + dde);
+        System.out.println("double dde = " + dde);
+
+        System.out.println("\n");
+
+        int re = 42;
+        double se = 42.25;
+        System.out.println("re mod 10 = " + re % 10);
+        System.out.println("se mod 10 = " + se % 10);
+
+        System.out.println("\n");
+
+        int g = 1;
+        int e = 2;
+        int l = 3;
+
+        g += 5;
+        e *= 4;
+        l += g * e;
+        l %= 6;
+        System.out.println("g = " + g);
+        System.out.println("e = " + e);
+        System.out.println("l = " + l);
+
+        System.out.println("\n");
+
+        int kit = 1;
+        int verter = 2;
+        int map;
+        int hee;
+        map = ++verter;
+        hee = kit++;
+        map++;
+        System.out.println("kit = " + kit);
+        System.out.println("verter = " + verter);
+        System.out.println("map = " + map);
+        System.out.println("hee = " + hee);
+
+        System.out.println("\n");
+
+        String binary[] = {
+            "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
+            "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"
+        };
+
+        int the = 3;
+        int man = 6;
+        int nos = the | man;
+        int turbo = the & man;
+        int exhaust = the ^ man;
+        int pipes = (~the & man) | (the & ~man);
+        int cyclinder = ~the & 0x0f;
+
+        System.out.println("    the = " + binary[the]);
+        System.out.println("    man = " + binary[man]);
+        System.out.println("the | man  = " + binary[nos]);
+        System.out.println("the & man = " + binary[turbo]);
+        System.out.println("the ^ man = " + binary[exhaust]);
+        System.out.println("~the * man | the * ~man = " + binary[pipes]);
+        System.out.println("~the & 0x0f = " + binary[cyclinder]);
+
+        System.out.println("\n");
+
+        byte companies = 64,p;
+        int w;
+        w = companies << 2;
+        p = (byte) (companies << 2);
+
+        System.out.println("Original value of companies: " + companies);
+        System.out.println("w and p: " + w + " " + p);
+    
